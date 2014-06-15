06a2a9f2fd7d809ddcc2d4798b96ff9795a3d5d5
Jakub Tomalak <jtomalak@gmail.com>
2014-06-15 20:39:09 +1000

    Removed any unwanted features and functions

--- lp_page_gen.py
+++ lp_page_gen.py
@@ -2,11 +2,7 @@
 
 from __future__ import print_function
 
-# TODO: UTF-8 support isn't ideal in Python,
-# so let's just ensure everything uses it by default
 import sys
-#reload(sys)
-#sys.setdefaultencoding('utf-8')
 
 import xml.etree.ElementTree as xml_parser
 import argparse as ap
@@ -17,9 +13,7 @@ from collections import OrderedDict
 '''
     Lonely Planet XML to HTML Generator
 '''
-# TODO! write up a quick XML generator to create a much LARGER set of sample files to better profile cpu/mem usage
 
-# TODO: refactor into a nicer top-level function with kwargs maybe
 def walk(node, f_op = lambda node: None, f_include = lambda node: True, parent = None, depth = 0):
     if f_include(node):
         f_op(node, parent, depth)
@@ -29,25 +23,6 @@ def walk(node, f_op = lambda node: None, f_include = lambda node: True, parent =
         c_parent = node if f_include(node) else parent
 
         walk(child, f_op, f_include, c_parent, c_depth)
-
-def print_taxonomy_node(node, parent, depth):
-    prefix = "   " * depth
-    parent_name = parent.find('node_name').text if depth != 0 else "root"
-    print("[", depth, "]", prefix, node.find('node_name').text, "=>", parent_name)
-
-# Wrap strings that look like a URL starting with 'www.' in a href prefixed with 'http://'.
-# TODO/FIXME: This will also turn http://www. into an href but with two http:// prefixes.
-# TODO/FIXME: Doing a replace of the match will catch addresses with the same base but different sub-pages.
-def find_and_convert_url_to_href(html_text):
-    if not html_text: return None
-
-    www_finder = re.compile(r'www\.[a-zA-Z0-9\./_-]*') # TODO unhandled chars: ~:?#\[\]@!$&()\*+,;=
-    www_matches = www_finder.findall(html_text)
-    for match in www_matches:
-        href_url = '<a href="http://' + match + '" target="_blank">' + match + '</a>'
-        html_text = html_text.replace(match, href_url)
-
-    return html_text
 
 class DestinationTemplatePopulator:
     def __init__(self, template_file, output_directory = './html_result'):
@@ -162,10 +137,8 @@ def main():
         htmlizer = TaxonomyNodeHtmlizer(
             DestinationContentGenerator(destinations_tree),
             DestinationTemplatePopulator('lp_template.html', args.output_directory)
-                .add_content_postprocessor(find_and_convert_url_to_href)
         )
         walk(taxonomy_tree.getroot(), htmlizer, htmlizer.valid_taxonomy_node)
-        #walk(taxonomy_tree.getroot(), print_taxonomy_node, htmlizer.valid_taxonomy_node)
 
     except IOError as ex:
         print("XML Parsing Error:", str(ex))
